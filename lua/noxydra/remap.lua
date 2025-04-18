1e5b7676679f117f57f420e4f8042d8c36b21e35
Lennart Frenzel <lenni.frenzel@gmail.com>
2025-04-18 17:27:12 +0200

    replaces nvimtree with neotree plugin

--- lua/noxydra/remap.lua
+++ lua/noxydra/remap.lua
@@ -4,7 +4,7 @@ vim.g.mapleader = " "
 --vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 --opens nvim-tree, activate when plugin is used
-vim.keymap.set("n", "<leader>pv", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>pv", ":Neotree toggle<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
@@ -30,3 +30,10 @@ end)
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>l", "<cmd>Lazy<CR>")
+
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
